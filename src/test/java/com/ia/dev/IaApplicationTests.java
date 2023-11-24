88c9d583f07e86e984156733f6b329694b0c0c19
hwan2-99 <thh1233123@gmail.com>
2023-11-24 13:48:23 +0900

    Initial commit

---
+++ src/test/java/com/ia/dev/IaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
